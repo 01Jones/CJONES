cc254fab8aed8e68c102761e48b004f3a6845021
C.Jones <94568312+01Jones@users.noreply.github.com>
2024-12-03 19:41:51 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -48,7 +48,7 @@ if option == 'Twitter':
     
     def scrape_twitter_mentions(ticker):
         # Example URL, this will not work for Twitter as it uses dynamic content
-        url = f"https://twitter.com/search?q=${ticker}&src=typed_query"
+        url = f"https://finviz.com/quote.ashx?t={ticker}"
         
         # Sending a request to the website
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
