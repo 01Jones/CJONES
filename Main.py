e2c8e3f4bb663ee424bf4ec150b222cdbe31a903
C.Jones <94568312+01Jones@users.noreply.github.com>
2024-12-03 18:22:53 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -25,21 +25,24 @@ access_token_secret = "GGJcu8ksG3lSrnlOa4vl3xWn7uFhD0eO1uyRA4Mwn08Vx"
 
 # Sidebar Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Select a page", ["Home", "About", "Contact"])
+page = st.sidebar.radio("Select a page", ["Home", "Watchlist", "Twitter Tracker", "XXXXX"])
 
 # Display content based on page selection
 if page == "Home":
     st.title("Home Page")
     st.write("Welcome to the Home Page!")
 
-elif page == "About":
+elif page == "Watchlist":
     st.title("About Page")
     st.write("This is the About Page.")
 
-elif page == "Contact":
+elif page == "Twitter Tracker":
     st.title("Contact Page")
     st.write("This is the Contact Page. You can provide contact details here.")
 
+elif page == "XXXXX":
+    st.title("Contact Page")
+    st.write("This is the Contact Page. You can provide contact details here.")
 
 
 
