4826f4e93ff183ee1691cb29e9d0bc018d73bf55
C.Jones <94568312+01Jones@users.noreply.github.com>
2024-12-05 07:56:55 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ access_token_secret = "GGJcu8ksG3lSrnlOa4vl3xWn7uFhD0eO1uyRA4Mwn08Vx"
 
 
 # Sidebar Navigation
-st.sidebar.markdown('Navigation</h1>')
+st.sidebar.markdown('Navigation')
 option = st.sidebar.radio("Select a page", ('Home', 'Watchlist', 'Twitter', 'Performance', 'Company Insights', 'Comparison Analysis', 'Framework')) 
 
 
